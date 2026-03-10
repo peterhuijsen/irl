8d2e4d3e04b22e3e2902345fcac9a9b4c05c63e3
Peter Huijsen <peter.huijsen@gmail.com>
2026-03-10 09:17:42 +0100

    feat: added assignment 2

---
+++ assignment_2/ShortCutAgents.py
@@ -0,0 +1,106 @@
+class QLearningAgent(object):
+
+    def __init__(self, n_actions, n_states, epsilon=0.1, alpha=0.1, gamma=1.0):
+        self.n_actions = n_actions
+        self.n_states = n_states
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.gamma = gamma
+        # TO DO: Initialize variables if necessary
+        
+    def select_action(self, state):
+        # TO DO: Implement policy
+        action = None
+        return action
+        
+    def update(self, state, action, reward, done): # Augment arguments if necessary
+        # TO DO: Implement Q-learning update
+        pass
+    
+    def train(self, n_episodes):
+        # TO DO: Implement the agent loop that trains for n_episodes. 
+        # Return a vector with the the cumulative reward (=return) per episode
+        episode_returns = []
+        return episode_returns
+
+
+class SARSAAgent(object):
+
+    def __init__(self, n_actions, n_states, epsilon=0.1, alpha=0.1, gamma=1.0):
+        self.n_actions = n_actions
+        self.n_states = n_states
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.gamma = gamma
+        # TO DO: Initialize variables if necessary
+        
+    def select_action(self, state):
+        # TO DO: Implement policy
+        action = None
+        return action
+        
+    def update(self, state, action, reward, done): # Augment arguments if necessary
+        # TO DO: Implement SARSA update
+        pass
+
+    def train(self, n_episodes):
+        # TO DO: Implement the agent loop that trains for n_episodes. 
+        # Return a vector with the the cumulative reward (=return) per episode
+        episode_returns = []
+        return episode_returns
+
+
+class ExpectedSARSAAgent(object):
+
+    def __init__(self, n_actions, n_states, epsilon=0.1, alpha=0.1, gamma=1.0):
+        self.n_actions = n_actions
+        self.n_states = n_states
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.gamma = gamma
+        # TO DO: Initialize variables if necessary
+        
+    def select_action(self, state):
+        # TO DO: Implement policy
+        action = None
+        return action
+        
+    def update(self, state, action, reward, done): # Augment arguments if necessary
+        # TO DO: Implement Expected SARSA update
+        pass
+
+    def train(self, n_episodes):
+        # TO DO: Implement the agent loop that trains for n_episodes. 
+        # Return a vector with the the cumulative reward (=return) per episode
+        episode_returns = []
+        return episode_returns    
+
+
+class nStepSARSAAgent(object):
+
+    def __init__(self, n_actions, n_states, n, epsilon=0.1, alpha=0.1, gamma=1.0):
+        self.n_actions = n_actions
+        self.n_states = n_states
+        self.n = n
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.gamma = gamma
+        # TO DO: Initialize variables if necessary
+        
+    def select_action(self, state):
+        # TO DO: Implement policy
+        action = None
+        return action
+        
+    def update(self, states, actions, rewards, done): # Augment arguments if necessary
+        # TO DO: Implement n-step SARSA update
+        pass
+    
+    def train(self, n_episodes):
+        # TO DO: Implement the agent loop that trains for n_episodes. 
+        # Return a vector with the the cumulative reward (=return) per episode
+        episode_returns = []
+        return episode_returns  
+    
+    
+    
